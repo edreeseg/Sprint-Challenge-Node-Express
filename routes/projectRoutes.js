4dd304912549a2e8ce87c37ff5879f65392a8533
edreeseg <edward.reeseg@gmail.com>
2019-03-09 13:04:00 -0500

    Project routes complete

---
+++ routes/projectRoutes.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../data/helpers/projectModel');
+
+router.get('/', (req, res) => {
+  db.get()
+    .then(projects => res.json({ projects }))
+    .catch(error => res.status(500).json({ error }));
+});
+
+router.post('/', (req, res) => {
+  const { name, description, completed } = req.body;
+  const entry = { name, description, completed };
+  if (Object.values(entry).some(x => x === undefined))
+    return res.status(400).json({
+      error: 'Request must include name, description, and completed keys.',
+    });
+  db.insert(entry)
+    .then(project => res.status(201).json({ project }))
+    .catch(error => res.status(500).json({ error }));
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  db.get(id)
+    .then(project => res.json({ project }))
+    .catch(error => {
+      if (error.message === "Cannot set property 'actions' of undefined")
+        return res
+          .status(404)
+          .json({ error: 'No project with that ID found.' });
+      else res.status(500).json({ error });
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, description, completed } = req.body;
+  const entry = { name, description, completed };
+  if (Object.values(entry).some(x => x === undefined))
+    return res.status(400).json({
+      error: 'Request must include name, description, and completed keys.',
+    });
+  db.update(id, entry)
+    .then(project => {
+      if (project === null)
+        return res
+          .status(404)
+          .json({ error: 'No project with that ID found.' });
+      else return res.json({ project });
+    })
+    .catch(error => res.status(500).json({ error }));
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  db.remove(id)
+    .then(count => {
+      if (count) return res.json({ success: 'Project successfully deleted.' });
+      else
+        return res
+          .status(404)
+          .json({ error: 'No project with that ID found.' });
+    })
+    .catch(error => res.status(500).json({ error }));
+});
+
+module.exports = router;
